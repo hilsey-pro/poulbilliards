68a11a4c7194a5690b3e69ad3c424fddaf22610a
hilsey-pro <mataliganawilliam@gmail.com>
2026-03-17 13:33:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function login() {
 function goTo(section) {
   alert(section + " page coming next");
 }
+function buyItem(itemName, price) {
+  alert(`Purchased ${itemName} for Tsh ${price}`);
+}
+
+function searchPapers() {
+  let input = document.getElementById("search").value.toLowerCase();
+  let cards = document.querySelectorAll(".paper-card");
+
+  cards.forEach(card => {
+    let text = card.innerText.toLowerCase();
+    card.style.display = text.includes(input) ? "block" : "none";
+  });
+}
+
+function exportPDF() {
+  alert("Export to PDF feature coming soon (needs backend library)");
+}
